270e9784ac21ff341319dc034bb64292cc9b1d39
Elgar de Groot <elgar.groot@gmail.com>
2018-11-14 18:01:49 +0100

    experimented with different koch directions; only draw when one of the orders actually changes

--- dragon.py
+++ dragon.py
@@ -1,15 +1,24 @@
-import sys
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import collections  as mc
 from matplotlib.widgets import Slider, Button
 
-sin_60 = np.sin(np.radians(60))
-cos_60 = np.cos(np.radians(60))
+dragon_order = 0
+koch_order = 0
+mirror = False
+
+if mirror:
+	sin_60 = np.sin(np.radians(-60))
+	cos_60 = np.cos(np.radians(-60))
+else:
+	sin_60 = np.sin(np.radians(60))
+	cos_60 = np.cos(np.radians(60))
+
 rotate_60 = np.array([[cos_60,sin_60],[-sin_60,cos_60]])
 rotate_90 = np.array([(0.0,1.0), (-1.0,0.0)])
 
 start_line = np.array([[[0.0,0.0],[1.0,0.0]]])
+
 
 def koch(line, order):
 	if order == 0:
@@ -30,11 +39,27 @@ def koch(line, order):
 	return np.concatenate((res0, res1, res2, res3))
 
 
-def dragon(lines, order):
+def mirror_koch(lines, dragon_iterations):
+	
+	if mirror and koch_order > 0:
+		for d in range(2**dragon_iterations):
+			base_line = lines[d*4**koch_order]
+			for k in range(d*4**koch_order + 1, (d+1)*4**koch_order):
+				line = lines[k]
+				if base_line[0,0] == base_line[1,0]: # a.x equals b.x so its a vertical line
+					line[0,0] = 2 * base_line[0,0] - line[0,0]
+					line[1,0] = 2 * base_line[1,0] - line[1,0]
+				elif base_line[0,1] == base_line[1,1]: # a.y equals b.y so its a horizontal line
+					line[0,1] = 2 * base_line[0,1] - line[0,1]
+					line[1,1] = 2 * base_line[1,1] - line[1,1]
+					
+
+def dragon(lines):
 	origin = [0.0,0.0]
-	for i in range(order):
+	for i in range(dragon_order):
 		rotated_lines = np.dot(lines-origin, rotate_90) + origin
-		origin = rotated_lines[0][1]
+		mirror_koch(rotated_lines, i)
+		origin = rotated_lines[0,1]
 		lines = np.vstack([lines, rotated_lines])
 	return lines
 
@@ -52,10 +77,18 @@ ax.set_aspect('equal')
 plt.axis('off')
 plt.tight_layout()
 
-def draw(val):
-	koch_order = int(np.round(koch_slider.val))
-	dragon_order = int(np.round(dragon_slider.val))
-	lines = dragon(koch(start_line, koch_order), dragon_order)
+def on_change(val):
+	global koch_order, dragon_order
+	k_order = int(np.round(koch_slider.val))
+	d_order = int(np.round(dragon_slider.val))
+	if k_order != koch_order or d_order != dragon_order:
+		koch_order = k_order
+		dragon_order = d_order
+		draw()
+
+def draw():
+	global koch_order, dragon_order
+	lines = dragon(koch(start_line, koch_order))
 	ax.set_xlim((np.amin(lines[:,:,0])-margin, np.amax(lines[:,:,0])+margin))
 	ax.set_ylim((np.amin(lines[:,:,1])-margin, np.amax(lines[:,:,1])+margin))
 	
@@ -68,10 +101,12 @@ def draw(val):
 
 koch_slider_axes  = fig.add_axes([0.25, 0.1, 0.65, 0.03])
 koch_slider = Slider(koch_slider_axes, 'Koch', 0, 6, valinit=0, valfmt='%0.0f')
-koch_slider.on_changed(draw)
+koch_slider.on_changed(on_change)
 
 dragon_slider_axes = fig.add_axes([0.25, 0.05, 0.65, 0.03])
 dragon_slider = Slider(dragon_slider_axes, 'Dragon', 0, 14, valinit=0, valfmt='%0.0f')
-dragon_slider.on_changed(draw)
+dragon_slider.on_changed(on_change)
+
+draw()
 
 plt.show()
